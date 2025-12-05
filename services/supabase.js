7b66b27c0928afe3e7ca58006ede4a1f6ee6d58f
hanjishi <dalisay.john06@gmail.com>
2025-12-05 18:02:54 +0800

    the access for DB

--- services/supabase.js
+++ services/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://onxvdbyachwukuyvnhyx.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ueHZkYnlhY2h3dWt1eXZuaHl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNTcwNzcsImV4cCI6MjA3OTkzMzA3N30.2OI51nMS4bMq1Tedfc7lib4IAh8UjsStlhz0lxqOmRw";
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+console.log("URL:", process.env.EXPO_PUBLIC_SUPABASE_URL);
+console.log("KEY:", process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
